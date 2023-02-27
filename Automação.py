9acf979ede44abdabb194c21aa209902f8161d41
Jardeilsom Oliveira <92388799+Jardeilso@users.noreply.github.com>
2023-02-26 21:04:53 -0300

    Add files via upload

---
+++ Automação.py
@@ -0,0 +1,15 @@
+import pyautogui
+from time import sleep
+from mouseinfo import mouseInfo
+mouseInfo()
+pyautogui.click(1670,116, duration=4)
+# 1. Clicar e digitar usuario 
+pyautogui.click(1161,450, duration=3)
+pyautogui.write('blakjd2@hotmail.com')
+pyautogui.click(1136,525, duration=3)
+pyautogui.write('83281983')
+pyautogui.click(1243,591, duration=3)
+#clicar em ok mudar senha
+pyautogui.click(1195,425, duration=3)
+pyautogui.click(724,821, duration=3)
+pyautogui.click(1647,426, duration=3)
